fc55b552d857f3609f8b03e0020fdeb502b791c6
dipak1920basnet <dipak1920basnet@gmail.com>
2025-04-01 15:09:30 +0545

    creates a table for a category

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     pass
+
+#Models for Category
+
+class Category(models.Model):
+    category_field = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.category_field}"
+    
