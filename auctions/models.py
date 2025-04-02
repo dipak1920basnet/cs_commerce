72a5f8ce04079fc69058b705182373110c0ccde4
dipak1920basnet <dipak1920basnet@gmail.com>
2025-04-02 13:44:58 +0545

    made model to store auction listing data

--- auctions/models.py
+++ auctions/models.py
@@ -12,3 +12,25 @@ class Category(models.Model):
     def __str__(self):
         return f"{self.category_field}"
     
+# Models for acution_listings
+class Auction_listing(models.Model):
+    
+    # a title for the listing,
+    title = models.CharField(max_length=30)
+    #  a text-based description, 
+    description = models.CharField(max_length=300) 
+    # what the starting bid should be.
+    starting_bid = models.IntegerField(blank=False)
+    # Users should also optionally be able to provide a URL for an image
+    image_url = models.URLField(null=True, blank=True)
+
+    # and/or a category
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='user')
+    is_active = models.BooleanField(default=True)
+    # comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name="comments")
+    watchlist = models.ManyToManyField(User, blank=True, null=True, related_name='onwatchlist')
+
+    def __str__(self):
+        return f"{self.title} {self.description}"
+    
