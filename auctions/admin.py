c5d13ed35825ed88fa11bdb76d4431e4ee2911de
dipak1920basnet <dipak1920basnet@gmail.com>
2025-04-02 14:24:45 +0545

    deleted some item

--- auctions/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Category, User, Auction_listing 
-
-# Register your models here.
-
-admin.site.register(User)
-admin.site.register(Category)
-admin.site.register(Auction_listing)
